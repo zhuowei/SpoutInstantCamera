7025a6b87b5aded722a0b68f4f5b4180943cfe10
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2012-01-04 23:48:12 -0800

    Ignore more blocks on right click

--- src/main/java/net/zhuoweizhang/spoutinstantcamera/CameraItem.java
+++ src/main/java/net/zhuoweizhang/spoutinstantcamera/CameraItem.java
@@ -1,11 +1,11 @@
 package net.zhuoweizhang.spoutinstantcamera;
 
-import org.bukkit.block.BlockFace;
-import org.bukkit.block.ContainerBlock;
+import org.bukkit.block.*;
 
 import org.getspout.spoutapi.block.SpoutBlock;
 import org.getspout.spoutapi.player.SpoutPlayer;
 import org.getspout.spoutapi.material.item.GenericCustomItem;
+import org.bukkit.Material;
 
 public class CameraItem extends GenericCustomItem {
 
@@ -18,7 +18,12 @@ public class CameraItem extends GenericCustomItem {
 
 	@Override
 	public boolean onItemInteract(SpoutPlayer player, SpoutBlock block, BlockFace face) {
-		if (plugin.ignoreRightClickOnContainers && block.getState() instanceof ContainerBlock) return false;
+		if (plugin.ignoreRightClickOnContainers && 
+			(block.getState() instanceof ContainerBlock || block.getState() instanceof NoteBlock || 
+				block.getState() instanceof Jukebox || block.getType() == Material.WOODEN_DOOR ||
+				block.getType() == Material.IRON_DOOR_BLOCK || block.getType() == Material.TRAP_DOOR ||
+				block.getType() == Material.DRAGON_EGG))
+			return false; //Did I miss anything?
 		return plugin.onTakePicture(player);
 	}
 
